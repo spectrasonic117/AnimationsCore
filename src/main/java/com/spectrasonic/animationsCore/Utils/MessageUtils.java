02a6c228ec340389d11303bf2cda80cd60a7e273
Manuel Heredia <Spectrasonic117@gmail.com>
2024-12-20 13:51:12 -0600

    ⭐ - 1.0.3-SNAPSHOT new MessageUtils System

--- src/main/java/com/spectrasonic/animationsCore/Utils/MessageUtils.java
+++ src/main/java/com/spectrasonic/animationsCore/Utils/MessageUtils.java
@@ -1,62 +1,99 @@
 package com.spectrasonic.animationsCore.Utils;
 
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.TextDecoration;
+import net.kyori.adventure.text.minimessage.MiniMessage;
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.Nullable;
 
 public final class MessageUtils {
-
-    public static final String DIVIDER = "----------------------------------------";
-    public static final String PREFIX = "&7[&6 PLUGINNAME &7] &6»&r ";
+    private static final Component DIVIDER = Component.text("----------------------------------------", NamedTextColor.GRAY);
+    private static final Component PREFIX = Component.text()
+            .append(Component.text("[", NamedTextColor.GRAY))
+            .append(Component.text("AnimationCore", NamedTextColor.GOLD))
+            .append(Component.text("] ", NamedTextColor.GRAY))
+            .append(Component.text("» ", NamedTextColor.GOLD))
+            .build();
 
     private MessageUtils() {
         // Private constructor to prevent instantiation
     }
 
-    public static void sendMessage(CommandSender sender, String message) {
-        sender.sendMessage(colorize(PREFIX + message));
+    public static void sendMessage(CommandSender sender, Component message) {
+        sender.sendMessage(PREFIX.append(message));
     }
 
-    public static void sendMessage(CommandSender sender, String message, Object... args) {
-        sender.sendMessage(colorize(PREFIX + String.format(message, args)));
+    public static void sendMessage(CommandSender sender, String miniMessage) {
+        sender.sendMessage(PREFIX.append(MiniMessage.miniMessage().deserialize(miniMessage)));
     }
 
-    public static void sendConsoleMessage(String message) {
-        Bukkit.getConsoleSender().sendMessage(colorize(PREFIX + message));
+    public static void sendConsoleMessage(Component message, CommandSender sender) {
+
+        sender.sendMessage(PREFIX.append(message));
     }
 
     public static void sendStartupMessage(JavaPlugin plugin) {
-        String[] messages = {
+        var pluginMeta = plugin.getPluginMeta();
+
+        Component[] messages = {
                 DIVIDER,
-                PREFIX + "&f" + plugin.getDescription().getName() + "&a Plugin Enabled!",
-                PREFIX + "&d" + "Version: &b" + plugin.getDescription().getVersion(),
-                PREFIX + "&f" + "Developed by: &c" + plugin.getDescription().getAuthors(),
+                PREFIX.append(Component.text()
+                        .append(Component.text(pluginMeta.getName(), NamedTextColor.WHITE))
+                        .append(Component.text(" Plugin Enabled!", NamedTextColor.GREEN))),
+                PREFIX.append(Component.text()
+                        .append(Component.text("Version: ", NamedTextColor.LIGHT_PURPLE))
+                        .append(Component.text(pluginMeta.getVersion(), NamedTextColor.AQUA))),
+                PREFIX.append(Component.text()
+                        .append(Component.text("Developed by: ", NamedTextColor.WHITE))
+                        .append(Component.text(String.join(", ", pluginMeta.getAuthors()), NamedTextColor.RED))),
                 DIVIDER
         };
 
-        for (String message : messages) {
-            Bukkit.getConsoleSender().sendMessage(colorize(message));
+        for (Component message : messages) {
+            plugin.getServer().getConsoleSender().sendMessage(message);
         }
     }
 
-    public static void broadcastMessage(String message) {
-        Bukkit.broadcastMessage(colorize(message));
+    public static void broadcastMessage(Component message) {
+        @Nullable Plugin plugin = Bukkit.getPluginManager().getPlugin("AnimationCore");
+        assert plugin != null;
+        plugin.getServer().broadcast(PREFIX.append(message));
     }
 
     public static void sendShutdownMessage(JavaPlugin plugin) {
-        String[] messages = {
+        Component[] messages = {
                 DIVIDER,
-                PREFIX + "&c" + plugin.getDescription().getName() + " plugin Disabled!",
+                PREFIX.append(Component.text()
+                        .append(Component.text(plugin.getPluginMeta().getName(), NamedTextColor.WHITE))
+                        .append(Component.text(" plugin Disabled!", NamedTextColor.RED))),
                 DIVIDER
         };
 
-        for (String message : messages) {
-            Bukkit.getConsoleSender().sendMessage(colorize(message));
+        for (Component message : messages) {
+            plugin.getServer().getConsoleSender().sendMessage(message);
         }
     }
 
-    private static String colorize(String message) {
-        return ChatColor.translateAlternateColorCodes('&', message);
+    // Método de utilidad para crear mensajes con formato personalizado
+    public static Component formatMessage(String text, NamedTextColor color) {
+        return Component.text(text, color);
+    }
+
+    // Método para crear mensajes con formato más complejo
+//    public static Component formatComplexMessage(String text, NamedTextColor color, TextDecoration... decorations) {
+//        return Component.text()
+//                .content(text)
+//                .color(color)
+//                .decorations(decorations)
+//                .build();
+//    }
+
+    // Método para mensajes con MiniMessage
+    public static Component parseMiniMessage(String miniMessage) {
+        return MiniMessage.miniMessage().deserialize(miniMessage);
     }
 }
